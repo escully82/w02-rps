7997c215b495c20ee3f131fd5e39ecba79da006e
Ed Scully <escully82@gmail.com>
2016-03-25 16:01:47 -0500

    working phase 1. 2 methods down

--- lib/rock_paper_scissors.rb
+++ lib/rock_paper_scissors.rb
@@ -1,46 +1,60 @@
 require "pry"
 
+# In this method, driver asks players for their names with gets.chomp and stores
+#    them for later use
+
+def ask_name
+  puts "\n\nSo, you there. What's your name? \n\n"
+  name = gets.chomp.capitalize
+  puts "\n\nThanks, #{name}!"
+  return name
+end
+
+##############################################################
+
+# In this method the driver asks wheter the players know the rules to the game
+
+def rules_knowledge_question(name)
+  puts "\n\n#{name} do you know the rules of rock, paper, scissors? \n\n"
+  answer = gets.chomp.downcase
+  return answer
+end
+
+##############################################################
+
 # The interface introduces the game to the players. (1. Greet players = done!)
+# =>                        GREETING
+# This is the BEGINNING! The FIRST thing that the user sees.
 
-puts "\n\nHello and welcome to the Hunger Games, where losing is a big deal!"
-
-##############################################################
-
-# Here I request the players names with gets.chomp. This input will be stored
-#   and used throughout the user/interface experience. I will also ask the
-#   players if they know the rules of the game, using gets.chomp.
-# This is steps 2 & 3. Done!
-
-puts "\n\nSo, you there. What's your name? \n\n"
-p1_name = gets.chomp
-puts "\n\nThanks, #{p1_name}!"
-puts "\n\nNow, what's your name? \n\n"
-p2_name = gets.chomp
-puts "\n\nThank you, #{p2_name}!\n\n"
-puts "\n\n#{p1_name}, do you know the rules of rock, paper, scissors? \n\n"
-p1_answer_1 = gets.chomp
-puts "\n\n#{p2_name}, how about you? \n\n"
-p2_answer_1 = gets.chomp
+puts "\n\nHello and welcome to the Hunger Games: Rock, Paper, Scissors Edition; where losing is a big deal!"
 
 ##############################################################
 
-# Array that is called if either or both of the players answer no to the
+# Interface asks for players one and twos name, respectively
+#
+# Refers to the ask_name method
+
+p1_name = ask_name
+
+p2_name = ask_name
+
+##############################################################
+
+# String that is called if either or both of the players answer no to the
 #   question, do you know the rules? Also, checks off #4 from our checklist!
 
 rules =
-[ "The rules are simple. You have three choices: rock, paper, or scissors. You get to choose one and only one. Are you keeping up so far? If not, then it's time for you to go home and open up your Kid Games for Dummmies book. Anyway, rock beats scissors, scissors beats paper, and paper beats rock. You can do best of three or best of five. That means winning two out of three games or winning three out of five games. Got it? That's it. It is that simple. Don't cheat. Cheating is frowned upon here. Ok now, #{p1_name} and #{p2_name}, let's get this party started!!!" ]
-
-rules.each do |rules|
-end
+ "The rules are simple. You have three choices: rock, paper, or scissors. You get to choose one and only one. Are you keeping up so far? If not, then it's time for you to go home and open up your Kid Games for Dummmies book. Anyway, rock beats scissors, scissors beats paper, and paper beats rock. You can do best of three or best of five. That means winning two out of three games or winning three out of five games. Got it? That's it. It is that simple. Don't cheat. Cheating is frowned upon here. Ok now, #{p1_name} and #{p2_name}, let's get this party started!!!"
 
 ##############################################################
 
-# Array of choices from which p1 and p2 can choose. That being: rock, paper, and scissors.
+# In this step I'm getting the answer to rules knowledge question
+#
+# Refers to the rules_knowledge_question method
 
-choices = ["rock", "paper", "scissors"]
-  choices.each do |input|
-end
+p1_answer_1 = rules_knowledge_question(p1_name)
 
+p2_answer_1 = rules_knowledge_question(p2_name)
 
 ##############################################################
 
@@ -63,11 +77,23 @@ end
 
 # We ask player for their move and get their input (5, 6)
 
+
 puts "\n\n\n#{p1_name}, we'll start with you. Which would you like to choose? \n\n"
-p1_move = gets.chomp
+p1_move = gets.chomp.downcase
+while p1_move != "rock" && p1_move != "paper" && p1_move != "scissors"
+  puts "\n\nSeriously #{p1_name}? That is not a valid move. Please enter: rock, paper, or scissors. How hard is that?\n\n"
+  p1_move = gets.chomp.downcase
+end
+
 puts "\n\n#{p2_name}, it's your turn. Which would you like to choose? \n\n"
-p2_move = gets.chomp
-puts "#{p1_move}, #{p2_move}"
+p2_move = gets.chomp.downcase
+while p2_move != "rock" && p2_move != "paper" && p2_move != "scissors"
+  puts "\n\nSeriously #{p2_name}? That is not a valid move. Please enter: rock, paper, or scissors. How hard is that?\n\n"
+  p2_move = gets.chomp.downcase
+end
+
+puts "\n\n#{p1_name} chose #{p1_move} and #{p2_name} chose #{p2_move}\n\n"
+
 
 ##############################################################
 
@@ -75,37 +101,24 @@ puts "#{p1_move}, #{p2_move}"
 #   rock, paper, scissors. These are: win, lose, or tie.
 
 if    p1_move == "rock"     && p2_move == "scissors"
-  puts result = "\n\nPlayer one wins!\n\n"
+  puts "\n\n#{p1_name} wins!!!\n\n"
 elsif p1_move == "rock"     && p2_move == "paper"
-  puts result = "\n\nPlayer two wins!\n\n"
+  puts "\n\n#{p2_name} wins!\n\n"
 elsif p1_move == "rock"     && p2_move == "rock"
-  puts result = "\n\nOh snap, that's a tie. Try again!\n\n"
+  puts "\n\nOh snap, #{p1_name} and #{p2_name} tied. Try again!\n\n"
 elsif p1_move == "scissors" && p2_move == "rock"
-  puts "\n\nPlayer two wins!\n\n"
+  puts "\n\n#{p2_name} wins!\n\n"
 elsif p1_move == "scissors" && p2_move == "paper"
-  puts "\n\nPlayer one wins\n\n"
+  puts "\n\n#{p1_name} wins\n\n"
 elsif p1_move == "scissors" && p2_move == "scissors"
-  puts "\n\nOh snap, that's a tie. Try again!\n\n"
+  puts "\n\nOh snap, #{p1_name} and #{p2_name} tied. Try again!\n\n"
 elsif p1_move == "paper"    && p2_move == "rock"
-  puts "\n\nPlayer one wins!\n\n"
+  puts "\n\n#{p1_name} wins!\n\n"
 elsif p1_move == "paper"    && p2_move == "scissors"
-  puts "\n\nPlayer two wins!\n\n"
+  puts "\n\n#{p2_name} wins!\n\n"
 elsif p1_move == "paper"    && p2_move == "paper"
-  puts "\n\nOh snap, that's a tie. Try again!\n\n"
-elsif result == "Player one wins!"
-  puts "\n\nCongratulations #{p1_name}!\n\n"
-elsif result == "Player two wins!"
-  puts "\n\nCongratulations #{p2_name}!\n\n"
-elsif result == "Oh snap, that's a tie. Try again!"
-  puts "\n\nOh snap, that's a tie. Try again!\n\n"
+  puts "\n\nOh snap, #{p1_name} and #{p2_name} tied. Try again!\n\n"
 end
 
 
-
 ##############################################################
-
-# Determine the result of the game
-
-#if p1_move
-
-#puts "\n\n\nOk #{}"
